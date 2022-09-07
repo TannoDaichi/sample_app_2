ef90b91b284e93a0eef6c6c1aeacdfe7606eea93
TannoDaichi <nekonindaichi15@gmail.com>
2022-09-07 04:55:40 +0000

    SAVE1_No.8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,14 +3,13 @@ class ListsController < ApplicationController
     @list = List.new  
   end
 
-  # 以下を追加
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to list_path(list.id)  
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end  
   end
   
   def index
@@ -33,7 +32,7 @@ class ListsController < ApplicationController
   
   def destroy
     list = List.find(params[:id])  # データ（レコード）を1件取得
-    list.delete  # データ（レコード）を削除
+    list.destroy  # データ（レコード）を削除
     redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
   end
   
